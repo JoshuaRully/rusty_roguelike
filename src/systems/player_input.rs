ed2dc4158932b774353d9f746557de9b880ce29c
Joshua Rully <joshrully@gmail.com>
2024-07-06 15:46:24 -0700

    [feature-items] Add item pickup

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -6,6 +6,8 @@ use crate::prelude::*;
 #[read_component(Player)]
 #[read_component(Enemy)]
 #[write_component(Health)]
+#[read_component(Item)]
+#[read_component(Carried)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -16,11 +18,27 @@ pub fn player_input(
     let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
 
     if let Some(key) = *key {
+        // TODO: change to WASD
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
             VirtualKeyCode::Right => Point::new(1, 0),
             VirtualKeyCode::Up => Point::new(0, -1),
             VirtualKeyCode::Down => Point::new(0, 1),
+            VirtualKeyCode::G => {
+                let (player, player_pos) = players
+                    .iter(ecs)
+                    .find_map(|(entity, pos)| Some((*entity, *pos)))
+                    .unwrap();
+                let mut items = <(Entity, &Item, &Point)>::query();
+                items
+                    .iter(ecs)
+                    .filter(|(_entity, _item, &item_pos)| item_pos == player_pos)
+                    .for_each(|(entity, _item, _item_pos)| {
+                        commands.remove_component::<Point>(*entity);
+                        commands.add_component(*entity, Carried(player));
+                    });
+                    Point::new(0, 0)
+            }
             _ => Point::new(0, 0),
         };
 
